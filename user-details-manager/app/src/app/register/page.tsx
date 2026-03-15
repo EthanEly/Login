142ca6413812d470b36c1d320d8d5c79a6909d7d
Ethan Ely <ethan@weconnectu.co.za>
2026-03-15 08:25:52 +0200

    Add register page w/ unit testing, validation and API client

---
+++ user-details-manager/app/src/app/register/page.tsx
@@ -0,0 +1,155 @@
+"use client";
+import { useState } from "react";
+import { UserFormData, UserRegistrationInformation, ValidationError } from "./models";
+import TextInput from "../../components/textInput";
+
+enum TextInputField {
+  FirstName = "first-name",
+  LastName = "last-name",
+  Email = "email-address",
+  Password = "password",
+  ConfirmPassword = "confirm-password",
+}
+
+export default function Register() {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const [errors, setErrors] = useState<ValidationError[]>([]);
+
+  const handleSubmit = async () => {
+    const validationErrors = validateFormData({
+      firstName,
+      lastName,
+      email,
+      password,
+      confirmPassword,
+    });
+    setErrors(validationErrors);
+
+    if (validationErrors.length === 0) {
+      await registerUser({ firstName, lastName, email, password });
+    }
+  };
+
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
+          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
+            Hello!
+          </h1>
+          <div className="max-w-md text-2xl text-zinc-600 dark:text-zinc-400">
+            <p className="max-w-md py-2 leading-8">First time? Let&apos;s get you registered!</p>
+            <TextInput
+              field={TextInputField.FirstName}
+              name="First name"
+              type="text"
+              value={firstName}
+              onChange={setFirstName}
+              errors={errors}
+            />
+            <TextInput
+              field={TextInputField.LastName}
+              name="Last name"
+              type="text"
+              value={lastName}
+              onChange={setLastName}
+              errors={errors}
+            />
+            <TextInput
+              field={TextInputField.Email}
+              name="Email address"
+              type="text"
+              value={email}
+              onChange={setEmail}
+              errors={errors}
+            />
+            <TextInput
+              field={TextInputField.Password}
+              name="Password"
+              type="password"
+              value={password}
+              onChange={setPassword}
+              errors={errors}
+            />
+            <TextInput
+              field={TextInputField.ConfirmPassword}
+              name="Confirm password"
+              type="password"
+              value={confirmPassword}
+              onChange={setConfirmPassword}
+              errors={errors}
+            />
+          </div>
+          <div className="flex flex-col text-base font-medium sm:flex-row">
+            <button
+              onClick={handleSubmit}
+              className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
+            >
+              Submit
+            </button>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export function validateFormData(userData: UserFormData): ValidationError[] {
+  const { firstName, lastName, email, password, confirmPassword } = userData;
+
+  const errors: ValidationError[] = [];
+
+  if (firstName.trim() === "") {
+    errors.push({ field: TextInputField.FirstName, message: "First name is required" });
+  }
+
+  if (lastName.trim() === "") {
+    errors.push({ field: TextInputField.LastName, message: "Last name is required" });
+  }
+
+  if (email.trim() === "") {
+    errors.push({ field: TextInputField.Email, message: "Email address is required" });
+  } else if (!email.includes("@")) {
+    errors.push({ field: TextInputField.Email, message: "Email address is invalid" });
+  }
+
+  if (password.trim() === "") {
+    errors.push({ field: TextInputField.Password, message: "Password is required" });
+  }
+
+  if (confirmPassword.trim() === "") {
+    errors.push({ field: TextInputField.ConfirmPassword, message: "Confirm password is required" });
+  }
+
+  if (password !== confirmPassword) {
+    errors.push({ field: TextInputField.ConfirmPassword, message: "Passwords do not match" });
+  }
+
+  return errors;
+}
+
+export async function registerUser(userData: UserRegistrationInformation) {
+  try {
+    const response = await fetch("/api/register", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(userData),
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    const result = await response.json();
+    return result;
+  } catch (error) {
+    throw error;
+  }
+}
---
+++ user-details-manager/app/tests/app/register.test.tsx
@@ -0,0 +1,223 @@
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
+import Register, { registerUser, validateFormData } from "../../src/app/register/page";
+import * as TextInputMock from "../../src/components/textInput";
+import { UserFormData, UserRegistrationInformation } from "@/src/app/register/models";
+
+describe("Landing Page", () => {
+  it("should render the page heading", () => {
+    render(<Register />);
+
+    const heading = screen.getByRole("heading", {
+      name: /Hello!/,
+    });
+
+    expect(heading).toBeVisible();
+  });
+
+  it("should call the text input component with the correct props", () => {
+    const textInputSpy = jest.spyOn(TextInputMock, "default");
+
+    render(<Register />);
+
+    expect(textInputSpy).toHaveBeenCalledWith(
+      {
+        errors: [],
+        field: "first-name",
+        name: "First name",
+        onChange: expect.any(Function),
+        type: "text",
+        value: "",
+      },
+      undefined,
+    );
+    expect(textInputSpy).toHaveBeenCalledWith(
+      {
+        errors: [],
+        field: "last-name",
+        name: "Last name",
+        onChange: expect.any(Function),
+        type: "text",
+        value: "",
+      },
+      undefined,
+    );
+    expect(textInputSpy).toHaveBeenCalledWith(
+      {
+        errors: [],
+        field: "email-address",
+        name: "Email address",
+        onChange: expect.any(Function),
+        type: "text",
+        value: "",
+      },
+      undefined,
+    );
+    expect(textInputSpy).toHaveBeenCalledWith(
+      {
+        errors: [],
+        field: "password",
+        name: "Password",
+        onChange: expect.any(Function),
+        type: "password",
+        value: "",
+      },
+      undefined,
+    );
+    expect(textInputSpy).toHaveBeenCalledWith(
+      {
+        errors: [],
+        field: "confirm-password",
+        name: "Confirm password",
+        onChange: expect.any(Function),
+        type: "password",
+        value: "",
+      },
+      undefined,
+    );
+  });
+
+  it("should render the submit button", () => {
+    render(<Register />);
+
+    const submitButton = screen.getByRole("button", {
+      name: /Submit/,
+    });
+
+    expect(submitButton).toBeVisible();
+  });
+
+  describe("validateFormData", () => {
+    const userData: UserFormData = {
+      firstName: "TEST_FIRST_NAME",
+      lastName: "TEST_LAST_NAME",
+      email: "TEST_EMAIL@EXAMPLE.COM",
+      password: "TEST_PASSWORD",
+      confirmPassword: "TEST_PASSWORD",
+    };
+
+    const testArrangement = [
+      {
+        userFormDataKey: "firstName",
+        fieldKey: "first-name",
+        expectedErrorMessage: "First name is required",
+      },
+      {
+        userFormDataKey: "lastName",
+        fieldKey: "last-name",
+        expectedErrorMessage: "Last name is required",
+      },
+      {
+        userFormDataKey: "email",
+        fieldKey: "email-address",
+        expectedErrorMessage: "Email address is required",
+      },
+      {
+        userFormDataKey: "password",
+        fieldKey: "password",
+        expectedErrorMessage: "Password is required",
+      },
+      {
+        userFormDataKey: "confirmPassword",
+        fieldKey: "confirm-password",
+        expectedErrorMessage: "Confirm password is required",
+      },
+    ];
+
+    it.each(testArrangement)(
+      "should return a validation error if field is not populated",
+      (testData) => {
+        const { userFormDataKey, fieldKey, expectedErrorMessage } = testData;
+        const userDataWithEmptyField: UserFormData = { ...userData, [userFormDataKey]: "" };
+
+        const errors = validateFormData(userDataWithEmptyField);
+
+        expect(errors).toEqual(
+          expect.arrayContaining([
+            {
+              field: fieldKey,
+              message: expectedErrorMessage,
+            },
+          ]),
+        );
+      },
+    );
+
+    it("should return a validation error if email is invalid", () => {
+      const userDataWithInvalidEmail = { ...userData, email: "invalid-email" };
+
+      const errors = validateFormData(userDataWithInvalidEmail);
+
+      expect(errors).toEqual(
+        expect.arrayContaining([
+          {
+            field: "email-address",
+            message: "Email address is invalid",
+          },
+        ]),
+      );
+    });
+
+    it("should return a validation error if password and confirm password do not match", () => {
+      const userDataWithInvalidEmail = {
+        ...userData,
+        password: "password",
+        confirmPassword: "not-matching-password",
+      };
+
+      const errors = validateFormData(userDataWithInvalidEmail);
+
+      expect(errors).toEqual(
+        expect.arrayContaining([
+          {
+            field: "confirm-password",
+            message: "Passwords do not match",
+          },
+        ]),
+      );
+    });
+  });
+
+  describe("registerUser", () => {
+    const userData: UserRegistrationInformation = {
+      firstName: "TEST_FIRST_NAME",
+      lastName: "TEST_LAST_NAME",
+      email: "TEST_EMAIL@EXAMPLE.COM",
+      password: "TEST_PASSWORD",
+    };
+
+    it("should POST to register endpoint with user data", async () => {
+      (global as any).fetch = jest.fn().mockResolvedValueOnce({
+        ok: true,
+        json: async () => ({ success: true }),
+      });
+
+      await registerUser(userData);
+
+      expect((global as any).fetch).toHaveBeenCalledWith("/api/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(userData),
+      });
+    });
+
+    it("throws an error when response is not ok", async () => {
+      (global as any).fetch = jest.fn().mockResolvedValueOnce({
+        ok: false,
+        status: 500,
+      });
+
+      await expect(registerUser(userData)).rejects.toThrow("HTTP error! Status: 500");
+    });
+
+    it("throws an error when fetch fails", async () => {
+      (global as any).fetch = jest.fn().mockImplementationOnce(() => {
+        throw new Error("Network error");
+      });
+
+      await expect(registerUser(userData)).rejects.toThrow("Network error");
+    });
+  });
+});
